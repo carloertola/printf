ba21d22d9051a98792b6cf0680c0bbb957f5a620
EugeneJoe <jmuthui70@gmail.com>
2021-03-13 16:59:53 +0000

    Add documentation for _printf function

---
+++ EC_printf.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _printf - Function that produces output according to a format.
+ *
+ * @format: a string composed of zero or more characters to print or use as
+ * directives that handle subsequent arguments and special characters.
+ *
+ * Description: This function can take a variable number and type of arguments
+ * that should be printed to standard output.
+ *
+ * Return: int
+ */
+int _printf(const char *format, ...)
+{
+}
