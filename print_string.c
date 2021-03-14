4a45a855763ee60ef516fc1ec1e936e37ae0f77d
EugeneJoe <jmuthui70@gmail.com>
2021-03-14 19:31:07 +0000

    Return no. of characters printed

--- print_string.c
+++ print_string.c
@@ -3,15 +3,21 @@
 /**
  * print_string - prints a string to stdout
  * @str: string to be printed
- * Return: no return value
+ * Return: no. of characters printed on success, or 0 on failure
  */
-void print_string(char *str)
+int print_string(char *str)
 {
-	int x = 0;
+	int x = 0, chars_printed = 0;;
 
 	while (*(str + x))
 	{
 		_putchar(*(str + x));
+		chars_printed++;
 		x++;
 	}
+
+	if (chars_printed > 0)
+		return (chars_printed);
+	else
+		return (0);
 }
