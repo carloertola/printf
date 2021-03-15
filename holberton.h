30aecc76d10ed54fb69c9c4ae95412b488734b4c
EugeneJoe <jmuthui70@gmail.com>
2021-03-15 20:40:53 +0000

    Modify print_num prototype

--- holberton.h
+++ holberton.h
@@ -7,7 +7,7 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_num(int);
+int print_num(long int n);
 int print_string(char *str);
 int print_binary(unsigned int num);
 int print_unknown_spec(char c);
