d12fee5d5dbfa1e2c3f79d1857963ac278b94647
Carlo Ertola <carloertola@gmail.com>
2021-03-15 18:50:46 +0000

    added a condition for the '%%' case

--- EC_printf.c
+++ EC_printf.c
@@ -32,8 +32,10 @@ int _printf(const char *format, ...)
 				chars_printed += print_string(va_arg(args, char *));
 			else if (format[i] == 'd' || format[i] == 'i')
 				chars_printed += print_num(va_arg(args, int));
-			else if (format[i] == 'b')
-				chars_printed += print_binary((unsigned int)va_arg(args, int));
+			else if (format[i] == '%')
+				chars_printed += _putchar('%');
+			/*else if (format[i] == 'b')
+				chars_printed += print_binary((unsigned int)va_arg(args, int));*/
 		}
 		i++;
 	}
