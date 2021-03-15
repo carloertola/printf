880a4f8ccc227a6041a13c6fef5f55cc014073ce
EugeneJoe <jmuthui70@gmail.com>
2021-03-15 17:35:40 +0000

    Convert int to binary and print it

---
+++ print_binary.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_binary - converts an unsigned int to binary
+ * @num: int to be converted
+ *
+ * Return: no of resulting binary characters
+ */
+int print_binary(unsigned int num)
+{
+	int chars_printed = 0, idx = 0;
+	char bin[sizeof(unsigned int) * 8];
+
+	while (num > 0)
+	{
+		bin[idx] = '0' + (num % 2);
+		num = num / 2;
+		idx++;
+	}
+	idx--;
+	while (idx >= 0)
+	{
+		chars_printed += _putchar(bin[idx]);
+		idx--;
+	}
+	return (chars_printed);
+}
