f7580f337710c6269246eb23266288b5a9d5d2b2
Carlo Ertola <carloertola@gmail.com>
2021-03-15 21:38:38 +0000

    added conditions for num == 0 and num == 1

--- print_binary.c
+++ print_binary.c
@@ -11,6 +11,10 @@ int print_binary(unsigned int num)
 	int chars_printed = 0, idx = 0;
 	char bin[sizeof(unsigned int) * 8];
 
+	if (num == 0)
+		chars_printed += _putchar('0');
+	if (num == 1)
+		chars_printed += _putchar('1');
 	while (num > 0)
 	{
 		bin[idx] = '0' + (num % 2);
