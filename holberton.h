9b0e2aa571453d3421f45c27658672372ba69fb5
Carlo Ertola <carloertola@gmail.com>
2021-03-16 10:50:12 +0000

    updated header file with functions to handle 'o', 'u', 'x' and 'X' specifiers

--- holberton.h
+++ holberton.h
@@ -11,6 +11,9 @@ int print_num(long int n);
 int print_string(char *str);
 int print_binary(unsigned int num);
 int print_unknown_spec(char c);
+int print_odh(char c, unsigned int num);
+int dec_to_oct(unsigned int num);
+int dec_to_hex(char c, unsigned int num);
 int print_reverse(char *s);
 int print_rot13(char *c);
 
