31a57dbd1b7cb21b62a18ab3e4a15c46eb3615b4
EugeneJoe <jmuthui70@gmail.com>
2021-03-16 11:03:47 +0000

    Print a reversed string

--- print_reverse.c
+++ print_reverse.c
@@ -7,18 +7,29 @@
  */
 int print_reverse(char *s)
 {
-	int x, y, chars_printed = 0;
+	int x, y, z, chars_printed = 0;
+	char *str;
 
 	x = 0;
 	y = 0;
+	z = 0;
 	if (s)
 	{
 		while (*(s + x))
 			x++;
+		str = malloc(sizeof(char) * x);
+		if (str == NULL)
+		{
+			free(str);
+			return (0);
+		}
 		for (y = (x - 1); y >= 0; y--)
 		{
-			chars_printed += _putchar(*(s + y));
+			*(str + z) = *(s + y);
+			z++;
 		}
+		*(str + x) = '\0';
+		chars_printed = print_string(str);
 		return (chars_printed);
 	}
 	return (0);
